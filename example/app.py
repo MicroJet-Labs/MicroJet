82204ac4064c05e3914c3c4ce6d539142db05c97
CODE GEAR <DuttaArin11@gmail.com>
2026-06-28 07:27:57 +0530

    Add files via upload

--- example/app.py
+++ example/app.py
@@ -1,5 +1,66 @@
-from atomy import App, Text
+from microjet import *
 
 app = App()
-app.route("/", Text("Welcome to Atomy.py"))
+
+page = Container(
+
+    Navbar(
+
+        NavbarBrand("MicroJet"),
+
+        NavbarMenu(
+            NavbarItem("Docs", href="/docs"),
+            NavbarItem("Components", href="/components"),
+            NavbarItem("GitHub", href="https://github.com/codegear&l&2011/MicroJet"),
+        )
+
+    ),
+Toast(
+    "The App Is Working!!",
+    variant="success",
+    
+),
+    Card(
+
+        
+
+    #Image(
+#        #src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0eLXMZJpj7CuIUoSfKGTHJn3hMFWaPIksjvSQ-27RT9T4wKuBshaJJeY&s=10",
+
+#        height="280px",
+#        width="100%",
+
+#        rounded="xl",
+
+#        fit="cover",
+
+#        shadow=True
+#    ),
+    Logo(150),
+
+    Text(
+        "The Install Worked Sucessfully,Congrats!!",
+        variant="large",font_size=35
+    ),
+    Text("What will you do next?", variant="lead"),
+    Text(
+    "1. Get started by editing app.py",
+    variant="default"
+    ),
+    Text(
+    "2. Read our DocsJet for learning MicroJet",
+    variant="default"
+    ),
+Button("Get Started",align="center"),
+Button("Documentation", variant="outline",align="center"),
+    ),
+    Text(""),
+    Text(
+        "© 2026 MicroJet • Developed for developers by Code Gear Pvt Ltd "
+    )
+
+)
+
+app.route("/", page)
+
 app.run()
